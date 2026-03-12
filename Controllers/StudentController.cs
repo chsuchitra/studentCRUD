2971097698d58320570d9abc4dd535a50dfa8a7e
chsuchitra <129029854+chsuchitra@users.noreply.github.com>
2026-03-12 13:18:10 +0530

    Add files via upload

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using StudentApi.Model;
+using StudentApi.Data;
+
+
+namespace StudentApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public StudentController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // INSERT
+        [HttpPost]
+        public IActionResult AddStudent(Student student)
+        {
+            _context.Students.Add(student);
+            _context.SaveChanges();
+            return Ok(student);
+        }
+
+        // UPDATE
+        [HttpPut("{id}")]
+        public IActionResult UpdateStudent(int id, Student student)
+        {
+            var existingStudent = _context.Students.Find(id);
+
+            if (existingStudent == null)
+                return NotFound();
+
+            existingStudent.Name = student.Name;
+            existingStudent.Age = student.Age;
+            existingStudent.City = student.City;
+
+            _context.SaveChanges();
+
+            return Ok(existingStudent);
+        }
+
+        // DELETE
+        [HttpDelete("{id}")]
+        public IActionResult DeleteStudent(int id)
+        {
+            var student = _context.Students.Find(id);
+
+            if (student == null)
+                return NotFound();
+
+            _context.Students.Remove(student);
+            _context.SaveChanges();
+
+            return Ok("Deleted Successfully");
+        }
+    }
+}
---
+++ Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using StudentApi.Model;     // using will import student.api--> project name , model --> folder
+using Microsoft.EntityFrameworkCore;    //it will help to ocnnect to db 
+
+namespace StudentApi.Data   // namespace is a container it will group related classes and libraries together
+{
+    public class AppDbContext : DbContext   // public it is a access modifier and can be accessed anywhere in file , inheritance 
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Student>Students{ get; set; }
+    }
+}
